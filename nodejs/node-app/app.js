c0dd58c9d1678c20732d9f9c3ca115fb0211e59c
snehadasa <sneha.dasasneha@gmail.com>
2020-01-05 08:56:14 -0800

    new packages

--- nodejs/node-app/app.js
+++ nodejs/node-app/app.js
@@ -1,8 +1,14 @@
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 const msg = getNotes()
 
 console.log(msg)
+
+console.log(chalk.green('Success'))
+
+const msgRed = chalk.red.inverse.italic('Sneha')
+console.log(msgRed)
 
 //const add = require('./utils.js')
 
